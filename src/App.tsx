d03bc1bacdeefb3ea5c9ee367b16d10c604a50c9
adityasuman2025 <adityasuman2025@gmail.com>
2023-09-17 09:58:51 +0530

    pdf viewer height increased

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,16 @@ import resume from './aditya_suman_sde2_iitp_resume.pdf';
 
 function App() {
     return (
-        <>
-            <MNgoSecurePDFViewer
-                securityOptions={{
-                    blockDownload: false,
-                    blockUserSelection: false,
-                    blockPrint: false,
-                    blockRightClick: false,
-                }}
-                pdfUrl={resume}
-                compHeight={"calc(100vh - 42px)"}
-            />
-        </>
+        <MNgoSecurePDFViewer
+            securityOptions={{
+                blockDownload: false,
+                blockUserSelection: false,
+                blockPrint: false,
+                blockRightClick: false,
+            }}
+            pdfUrl={resume}
+            compHeight={"100vh"}
+        />
     )
 }
 
