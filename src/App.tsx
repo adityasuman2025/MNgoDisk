a0dc97fe2db5ad5460f34936baaa51fdcf34e17c
adityasuman2025 <adityasuman2025@gmail.com>
2023-11-03 14:33:27 +0530

    trying 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,30 @@
 import { useState, useEffect } from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import RenderFile from "./RenderFile";
 import { apiCall, getFilePathToUrlMap } from "./utils";
+
 
 const FIREBASE_BASE_URL = "https://firebasestorage.googleapis.com/v0/b/documents-b4b54.appspot.com/o";
 const LOCAL_STORAGE_FB_FILES_KEY = "filesJSON";
 const LOCAL_STORAGE_FB_FILES = JSON.parse(localStorage.getItem(LOCAL_STORAGE_FB_FILES_KEY) || "[]");
 
-function getRoutes2(array: any[]): any[] {
-    return array.map(({ path, fileUrl }) => ({ path, element: <RenderFile fileUrl={fileUrl} /> }));
+// function getRoutes2(array: any[]): any[] {
+//     return array.map(({ path, fileUrl }) => ({ path, element: <RenderFile fileUrl={fileUrl} /> }));
+// }
+function getRoutes(array: any[]): any[] {
+    const routes = array.map(({ path, fileUrl }) => {
+        return <Route key={path} path={path} element={<RenderFile fileUrl={fileUrl} />} />
+    });
+
+    routes.push(<Route key={"*"} path={"*"} element={<RenderFile fileUrl={`${FIREBASE_BASE_URL}/aditya_suman_sde2_iitp.pdf?alt=media`} />} />);
+
+    return routes;
 }
 
 export default function App() {
-    const [routes, setRoutes] = useState<any[]>(getRoutes2(LOCAL_STORAGE_FB_FILES));
+    const [routes, setRoutes] = useState<any[]>(getRoutes(LOCAL_STORAGE_FB_FILES));
+    console.log("routes", routes)
 
     useEffect(() => {
         (async function () {
@@ -20,19 +32,27 @@ export default function App() {
                 const items = (await apiCall(FIREBASE_BASE_URL))?.items || [];
                 const firebaseFiles: any[] = getFilePathToUrlMap(items, FIREBASE_BASE_URL);
 
-                setRoutes(getRoutes2(firebaseFiles));
+                setRoutes(getRoutes(firebaseFiles));
                 localStorage.setItem(LOCAL_STORAGE_FB_FILES_KEY, JSON.stringify(firebaseFiles));
             } catch { }
         })();
     }, []);
 
-    const router = createBrowserRouter([
-        ...routes,
-        {
-            path: "*",
-            element: <RenderFile fileUrl={`${FIREBASE_BASE_URL}/aditya_suman_sde2_iitp.pdf?alt=media`} />,
-        }
-    ]);
+    return (
+        <BrowserRouter >
+            <Routes>
+                {routes}
+            </Routes>
+        </BrowserRouter>
+    );
 
-    return <RouterProvider router={router} />
+    // const router = createBrowserRouter([
+    //     ...routes,
+    //     {
+    //         path: "*",
+    //         element: <RenderFile fileUrl={`${FIREBASE_BASE_URL}/aditya_suman_sde2_iitp.pdf?alt=media`} />,
+    //     }
+    // ]);
+
+    // return <RouterProvider router={router} />
 }
